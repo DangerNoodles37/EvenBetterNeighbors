d3d3f8fab22bbec1c7ab6cfdbb0c91385ffbe9ae
Greg Jenner <jennergreg733@gmail.com>
2022-12-14 12:26:02 -0500

    add bcrypt to merchantModel

--- server/models/merchantModel.js
+++ server/models/merchantModel.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcryptjs');
 const SALT_WORK_FACTOR = 10;
 
 const merchantSchema = new mongoose.Schema({
-  businessEmail: { type: String, required: true },
+  businessEmail: { type: String, required: true, unique: true },
   merchantPassword: { type: String, required: true },
   businessName: { type: String, required: true },
   typeOfBusiness: { type: String, required: true },
@@ -12,6 +12,26 @@ const merchantSchema = new mongoose.Schema({
   businessZipCode: { type: String },
   description: { type: String },
   image: { type: Buffer },
+});
+
+merchantSchema.pre('save', function (next) {
+  const merchant = this;
+  bcrypt
+    .hash(merchant.merchantPassword, SALT_WORK_FACTOR)
+    .then((hash) => {
+      merchant.merchantPassword = hash;
+      return next();
+    })
+    .catch((err) =>
+      next({
+        log: `hash in merchantModel: ERROR: ${
+          typeof err === `object` ? JSON.stringify(err) : err
+        }`,
+        message: {
+          err: 'Error occurred in hash method of merchantModel',
+        },
+      })
+    );
 });
 
 module.exports = mongoose.model('merchant', merchantSchema);
