e6f54fa20feafea260af19fd22a205d50b16556b
Andrew Hogan <andrewlallyhogan@gmail.com>
2022-12-15 12:39:35 -0500

    merchant create should (?) be working

--- test/Backend/Routes.test.js
+++ test/Backend/Routes.test.js
@@ -8,19 +8,74 @@ describe('Requests to /createUser should work', () => {
     request(server)
     .post("/createUser")
     .send({
-      firstName: 'Andrew',
-      lastName: 'Hogan',
-      email: 'lalala',
-      password: 'camille',
-      zipCode: '123'
+      firstName: 'TESTFIRSTNAME2',
+      lastName: 'TESTLASTNAME2',
+      email: 'TESTEMAIL2',
+      password: 'TESTPASSWORD2',
+      zipCode: 'TESTZIPCODE2'
     })
+    .expect('Found. Redirecting to http://localhost:8080/loginPage')
     .expect(302)
+    .expect('Location', /\/loginPage/)
     .end((err, res) => {
       if (err) return done(err);
       return done();
     })
   });
 });
+
+// merchantEmail: { type: String, required: true, unique: true },
+//   merchantPassword: { type: String, required: true },
+//   merchantName: { type: String, required: true },
+//   typeOfMerchant: { type: String, required: true },
+//   merchantAddress: { type: String },
+//   merchantZipCode: { type: String },
+//   description: { type: String },
+//   image: {
+//     type: String,
+//     default: 'https://en.wiktionary.org/wiki/File:Cirila-%D0%9C-majuskla.svg',
+
+describe('requests to createMerchant should create a merchant in the dtb', () => {
+  test('POST /createMerchant', (done) => {
+    request(server)
+      .post('/createMerchant')
+      .send({
+        merchantEmail: 'testMerchantCheck',
+        merchantPassword: 'testMerchantPassword',
+        merchantName: 'checkCheck',
+        typeOfMerchant: 'checkTypeCheck',
+        merchantAddress: 'checkAddress',
+        merchantZipCode: 'checkThis',
+        description: 'whatitis',
+        image: 'checkImage'
+      })
+      .expect('Found. Redirecting to http://localhost:8080/loginPage')
+      .expect(302)
+      .expect('Location', /\/loginPage/)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Merchant.findOne({ merchantEmail: 'testMerchantCheck'})
+          .then((merchant) => {
+            expect(merchant).toBeTruthy();
+            return done();
+          })
+          .catch((err) => {
+            return done(err);
+          });
+        })
+        .catch((err) => {
+          return done(err);
+      });
+  });
+  // db.merchants.dropIndex("businessEmail_1")
+
+
+})
+
+// 'http://localhost:8080/loginPage'
 
 // const userSchema = new mongoose.Schema({
 //   firstName: { type: String, required: true },
@@ -31,32 +86,19 @@ describe('Requests to /createUser should work', () => {
 // });
 
 
-xdescribe('Requests to /test should work', () => {
-  test('Get /test', (done) => {
-    request(server)
-    .get("/test")
-    .expect(200)
-    .end((err, res) => {
-      if (err) return done(err);
-      return done();
-    })
-  });
-});
+// xdescribe('Requests to /test should work', () => {
+//   test('Get /test', (done) => {
+//     request(server)
+//     .get("/test")
+//     .expect(200)
+//     .end((err, res) => {
+//       if (err) return done(err);
+//       return done();
+//     })
+//   });
+// });
 
-describe('POST /createUser', () => {
-  it('responds with 302 status and redirects to /LoginPage', async () => {
-    const response = await request(server)
-      .post('/createUser')
-      .send({
-        username: 'testuser',
-        password: 'testpassword'
-
-      })
-      .expect('Location', '/LoginPage')
-      .expect(302);
-  });
-});
-
+//we'll come back to this
 xdescribe('DELETE /delete', () => {
   it('should return a status code of 200', async () => {
     const response = await request(server)
@@ -70,11 +112,116 @@ xdescribe('DELETE /delete', () => {
   });
 });
 
-describe('PATCH /update', () => {
-  it('should successfully update existing user', () => {
+xdescribe('PATCH /update', () => {
+  it('should successfully update the firstname field in the user schema', () => {
+    const req = {
+      body: {
+        firstName: 'John'
+      }
+    };
+    //res 
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    };
 
+    userControllers.updateUser(req,res);
+
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({});
   })
-})
+
+  xit('should update the lastName field in the user schema', () => {
+    const req = {
+      body: {
+        lastName: 'Joe'
+      }
+    };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    };
+
+    userControllers.updateUser(req, res);
+    
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({});
+  });
+
+  xit('should update the password field in the user schema', () => {
+    const req = {
+      body: {
+        password: 'newpassword123'
+      }
+    };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    };
+
+    userControllers.updateUser(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({});
+  });
+
+  xit('should update the email field in the user schema', () => {
+    
+    const req = {
+      body: {
+        email: 'cam.check@example.com'
+      }
+    };
+    
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    };
+
+    userControllers.updateUser(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({});
+  });
+
+  xit('should update the zipcode field in the user schema', () => {
+    const req = {
+      body: {
+        zipcode: '90210'
+      }
+    };
+
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn()
+    };
+
+    userControllers.updateUser(req, res);
+
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({});
+  });
+});
+
+xdescribe('GET /allMerchants', () => {
+  it('should return a 200 status code', async () => {
+    const res = await request(app).get('/allMerchants');
+    expect(res.statusCode).toBe(200);
+  });
+
+  xit('should return a list of merchants in the response body', async () => {
+    const res = await request(app).get('/allMerchants');
+    expext(Array.isArray(res.body)).toBe(true);
+    expect(res.body.length).toBeGreaterThan(0);
+  });
+
+  xit('should return the correct fields for each merchant in the response body', async () => {
+    const res = await request(app).get('/allMerchants');
+    //check all expected properties
+    expect(res.body[0]).toHaveProperty('');
+  });
+});
+
 
 
 
--- test/React/Userinformation.test.js
+++ test/React/Userinformation.test.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { render, screen, cleanup } from '@testing-library/react';
 import UserInformation from '../../client/src/UserInformation';
 
-test('it should render the UserInformation component', () => {
+xtest('it should render the UserInformation component', () => {
   render(<UserInformation />);
   const userInformation = screen.getByTestId('userInformation');
   expect(userInformation).toBeInTheDocument();
