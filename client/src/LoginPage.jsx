736b7d2cfc1cefba973f5d835da03817f95894dc
Greg Jenner <jennergreg733@gmail.com>
2022-12-15 16:15:14 -0500

    render merchants on landing from database, verify creation and loggin

--- client/src/LoginPage.jsx
+++ client/src/LoginPage.jsx
@@ -2,21 +2,15 @@ import React, { useState, useEffect } from 'react';
 import NavBar from './NavBar';
 import { useHistory } from 'react-router-dom';
 import axios from 'axios';
+
 function LoginPage(props) {
   const history = useHistory();
-  
+
   const handleLogin = () => {
-    console.log('hi')
-    axios({
-      method: 'get',
-      url: 'http://localhost:3000/allMerchants',
-    })
-      .then((responseData) => {
-        console.log(' All Merchants: ', responseData.data)
-        setMerchantData(responseData.data)
-    })
-    props.setNeedMerchantData(props.needMerchantData)
-  }
+    // event.preventDefault();
+    console.log('hi from the login button');
+    // props.setNeedMerchantData(props.needMerchantData)
+  };
 
   return (
     <div>
@@ -26,7 +20,7 @@ function LoginPage(props) {
       </div>
       <div className='loginContainer'>
         {/* hold both user and merchant login */}
-        
+
         <div className='consMerchContainer'>
           {/* user login */}
           <div className='userLogin'>
@@ -35,57 +29,32 @@ function LoginPage(props) {
             </div>
             <br />
             {/* USER LOGIN FORM GOES HERE */}
-            
+
             <div className='innerAlign'>
-            <form
-              onSubmit={handleLogin}
-              className='userForm'
-              method='post'
-              action='http://localhost:3000/userLogin'
-            >
-              {/* <form className='userForm' onSubmit={handleUserLogin}*/}
-              <div className='userLogin'>
-                {/* USER EMAIL INPUT */}
-                <label htmlFor='email'>Email: </label>
-                <input type='text' name='email' id='email' />
-
-                {/* <label htmlFor='userEmailLogin'>
-                  Email:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                  <input
-                    type='text'
-                    id='userEmailLogin'ss
-                    value={userEmailLogin}
-                    onChange={(event) =>
-                      setUserEmailLogin(event.target.value)
-                    }
-                  />
-                </label> */}
-              </div>
-
-              {/* USER PASSWORD INPUT */}
-              <div className='userPW'>
-                <label htmlFor='password'>Password: </label>
-                <input type='password' name='password' id='password' />
-
-                {/* <label htmlFor='userPasswordLogin'>
-                  Password:
-                  <input
-                    type='text'
-                    id='userPasswordLogin'
-                    value={userPasswordLogin}
-                    onChange={(event) => {
-                      setUserPasswordLogin(event.target.value);
-                    }}
-                  />
-                </label> */}
-              </div>
-              <input
-                name='userSubmit'
-                id='formButton'
-                type='submit'
-                value='submit'
-              />
-            </form>
+              <form
+                onSubmit={handleLogin}
+                className='userForm'
+                method='post'
+                action='http://localhost:3000/userLogin'
+              >
+                {/* <form className='userForm' onSubmit={handleUserLogin}*/}
+                <div className='userLogin'>
+                  {/* USER EMAIL INPUT */}
+                  <label htmlFor='email'>Email: </label>
+                  <input type='text' name='email' id='email' />
+                </div>
+                {/* USER PASSWORD INPUT */}
+                <div className='userPW'>
+                  <label htmlFor='password'>Password: </label>
+                  <input type='password' name='password' id='password' />
+                </div>
+                <input
+                  name='userSubmit'
+                  id='formButton'
+                  type='submit'
+                  value='submit'
+                />
+              </form>
             </div>
           </div>
 
@@ -96,18 +65,18 @@ function LoginPage(props) {
             </div>
             <br />
             <div className='innerAlign'>
-            <form
-              onSubmit={handleLogin}
-              className='merchantForm'
-              method='post'
-              action='http://localhost:3000/merchantLogin'
-            >
-              {/* MERCHANT EMAIL LOGIN */}
-              <div className='merchantUsername'>
-                <label htmlFor='memail'>Email: </label>
-                <input type='text' name='memail' id='memail' />
+              <form
+                onSubmit={handleLogin}
+                className='merchantForm'
+                method='post'
+                action='http://localhost:3000/merchantLogin'
+              >
+                {/* MERCHANT EMAIL LOGIN */}
+                <div className='merchantUsername'>
+                  <label htmlFor='memail'>Email: </label>
+                  <input type='text' name='memail' id='memail' />
 
-                {/* <label htmlFor='merchantUsernameLogin'>
+                  {/* <label htmlFor='merchantUsernameLogin'>
                   Email: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                 <input 
                 type='text' 
@@ -118,14 +87,14 @@ function LoginPage(props) {
                 }}
                 />
                 </label> */}
-              </div>
+                </div>
 
-              {/* MERCHANT PASSWORD LOGIN */}
-              <div className='merchantPassword'>
-                <label htmlFor='mpassword'>Password: </label>
-                <input type='password' name='mpassword' id='mpassword' />
+                {/* MERCHANT PASSWORD LOGIN */}
+                <div className='merchantPassword'>
+                  <label htmlFor='mpassword'>Password: </label>
+                  <input type='password' name='mpassword' id='mpassword' />
 
-                {/* <label htmlFor='merchantPasswordLogin'>
+                  {/* <label htmlFor='merchantPasswordLogin'>
                   Password:
                   <input
                     type='text'
@@ -136,13 +105,12 @@ function LoginPage(props) {
                     }}
                   />
                 </label> */}
-              </div>
+                </div>
 
-              <input id='merchantFormButton' type='submit' value='submit' />
-            </form>
+                <input id='merchantFormButton' type='submit' value='submit' />
+              </form>
             </div>
           </div>
-
         </div>
       </div>
     </div>
--- client/src/Modal.jsx
+++ client/src/Modal.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
-import styles from './Modal.modules.css'
+import React from 'react';
+import styles from './Modal.modules.css';
+
+// merchantEmail
+//
+// merchantPassword
+//
+// merchantName
+//
+// typeOfMerchant
+//
+// merchantAddress
+//
+// merchantZipCode
+//
+// description
+//
+// image
+// "https://en.wiktionary.org/wiki/File:Cirila-%D0%9C-majuskla.svg"
 
 // const Modal = (props) => {
-const Modal = ({merchantData, modalId, setIsOpen}) => {
-  console.log(merchantData)
-  console.log(modalId)
-
+const Modal = ({ merchantData, modalId, setIsOpen }) => {
+  console.log(merchantData);
+  console.log(modalId);
 
   return (
     <>
@@ -13,11 +29,21 @@ const Modal = ({merchantData, modalId, setIsOpen}) => {
       <div className={styles.centered}>
         <div className={styles.modal}>
           <div className={styles.modalHeader}>
-            <h1 className={styles.heading}>{merchantData[modalId].storeName}</h1>
+            <h1 className={styles.heading}>
+              {merchantData[modalId].merchantName}
+            </h1>
           </div>
-          <button className={styles.closeBtn} onClick={() => setIsOpen(false)}>X</button>
+          <button className={styles.closeBtn} onClick={() => setIsOpen(false)}>
+            X
+          </button>
           <div className={styles.modalContent}>
-            <a className={styles.address} href={`https://maps.google.com/?q=${merchantData[modalId].storeName} ${merchantData[modalId].address}`}>{merchantData[modalId].address}, {merchantData[modalId].zipCode}</a>
+            <a
+              className={styles.address}
+              href={`https://maps.google.com/?q=${merchantData[modalId].merchantName} ${merchantData[modalId].merchantAddress}`}
+            >
+              {merchantData[modalId].merchantAddress},{' '}
+              {merchantData[modalId].merchantZipCode}
+            </a>
           </div>
           <div className={styles.modalContent}>
             {merchantData[modalId].description}
@@ -25,14 +51,14 @@ const Modal = ({merchantData, modalId, setIsOpen}) => {
           <div className={styles.modalActions}>
             <div className={styles.actionsContainer}>
               <div className={styles.modalContent}>
-                Contact Us: {merchantData[modalId].email}
+                Contact Us: {merchantData[modalId].merchantEmail}
               </div>
             </div>
           </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Modal
+export default Modal;
